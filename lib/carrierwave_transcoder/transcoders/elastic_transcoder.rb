f6e26bc5c42a85fdd111d3fda094e8a8d79e3328
Jamie Wright <jamie@brilliantfantastic.com>
2017-10-10 15:15:59 -0400

    Implement default options

--- lib/carrierwave_transcoder/transcoders/elastic_transcoder.rb
+++ lib/carrierwave_transcoder/transcoders/elastic_transcoder.rb
@@ -1,10 +1,32 @@
 module CarrierWave
   module Transcoders
     class ElasticTranscoder
+      attr_reader :options
+
       def initialize(options)
+        @options = default_options_with(options.symbolize_keys)
       end
 
       def transcode
+      end
+
+      private
+
+      def default_options_with(options)
+        {
+          inputs: [{
+            key: "#{options[:store_dir]}/#{options[:filename]}"
+          }],
+          outputs: [{
+            key: "#{options[:basename]}#{options[:output_extension]}"
+          }]
+        }.merge(options) do |key, old_value, new_value|
+          if options[key].is_a?(Array)
+            [(old_value + new_value).reduce(Hash.new, :merge)]
+          else
+            new_value
+          end
+        end
       end
     end
   end
--- spec/transcoders/elastic_transcoder_spec.rb
+++ spec/transcoders/elastic_transcoder_spec.rb
@@ -1,6 +1,49 @@
 RSpec.describe CarrierWave::Transcoders::ElasticTranscoder do
+  let(:file_options) do
+    {
+      basename: "user",
+      content_type: "video/mp4",
+      extension: "mp4",
+      filename: "user.mp4",
+      store_dir: "uploads"
+    }
+  end
+  let(:options) do
+    {
+      pipeline_id: "pipeline-id",
+      inputs: [{
+        key: "uploads/some_file.mp4"
+      }],
+      outputs: [{
+        key: "some_smaller_file.webm",
+        preset_id: "preset-id"
+      }],
+      output_key_prefix: "transcoded/"
+    }
+  end
+
   describe "#initialize" do
-    xit "initializes with some default options"
+    it "initializes with some options" do
+      expect(described_class.new(options).options).to eq options
+    end
+
+    it "defaults the inputs key with the input file" do
+      options.delete(:inputs)
+
+      subject = described_class.new(file_options.merge(options))
+
+      expect(subject.options[:inputs][0][:key]).to eq "uploads/user.mp4"
+    end
+
+    it "defaults the outputs key with the input file and output extension" do
+      options.merge!(output_extension: ".webm")
+      options[:outputs][0].delete(:key)
+
+      subject = described_class.new(file_options.merge(options))
+
+      expect(subject.options[:outputs][0][:key]).to eq "user.webm"
+      expect(subject.options[:outputs][0][:preset_id]).to eq "preset-id"
+    end
   end
 
   describe "#transcode" do
